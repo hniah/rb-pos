c901786ca218777ff86f11494906091d591fbbbe
Anthony <anthony@futureworkz.com>
2015-04-10 13:05:45 +0700

    Add admin model

---
+++ app/models/admin.rb
@@ -0,0 +1,7 @@
+class Admin < ActiveRecord::Base
+  include RailsAdmin::Admin
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
---
+++ app/models/concerns/rails_admin/admin.rb
@@ -0,0 +1,22 @@
+module RailsAdmin::Admin
+  extend ActiveSupport::Concern
+
+  included do
+    rails_admin do
+      list do
+        field :email
+      end
+
+      show do
+        field :email
+        field :updated_at
+      end
+
+      edit do
+        field :email
+        field :password
+        field :password_confirmation
+      end
+    end
+  end
+end
