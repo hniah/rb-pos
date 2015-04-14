1a23b374956cef4a1b826f6e36484d05737394e2
Anthony <anthony@futureworkz.com>
2015-04-14 11:48:18 +0700

    Add spec for project and projectstep

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Project do
+  context 'validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :category_id }
+    it { is_expected.to validate_presence_of :user_id }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :cost }
+    it { is_expected.to validate_presence_of :time }
+    it { is_expected.to validate_numericality_of(:cost).only_integer }
+    it { is_expected.to validate_numericality_of(:difficult_level).is_greater_than(0) }
+    it { is_expected.to validate_presence_of :can_clone }
+  end
+end
---
+++ spec/models/project_steps_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ProjectStep do
+  context 'validations' do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :description }
+    it { is_expected.to validate_presence_of :project_id }
+  end
+end
